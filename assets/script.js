65b153666aba1275c8e0a99797604b2e7f1d645a
Joshua Goeke <113941639+joshuagoeke@users.noreply.github.com>
2023-01-11 11:59:48 -0600

    Update script.js
    Music API

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,20 @@ fetch('https://billboard3.p.rapidapi.com/hot-100?date=2022-07-07&range=1-10', op
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+
+
+//MUSIC API
+
+const options = {
+	method: 'GET',
+	headers: {
+		'Content-Type': 'application/json; charset=utf-8',
+		'X-RapidAPI-Key': '5efa58815emsh6280383a06b5a25p1cb931jsn08fce52522f3',
+		'X-RapidAPI-Host': 'musicapi13.p.rapidapi.com'
+	}
+};
+
+fetch('https://musicapi13.p.rapidapi.com/search/introspection', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
