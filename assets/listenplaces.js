507802999da91527a656a75b712fef9c39846d63
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-01-16 22:27:17 -0600

    ugly MVP, can click on a song from a list provided by Billboard, then get a list of places to listen to that song

--- assets/listenplaces.js
+++ assets/listenplaces.js
@@ -1,15 +1,27 @@
-//testDolly is enormous, so paste here to test, then delete. Minimize while testing for easier scrolling
-console.log("save the Functions!");
+console.log("hey hey hey!");
 
 //variables
-var searchObject = '{"track":"9 to 5","artist":"","type":"track","sources":["amazon-music","apple-music","deezer","pandora","sound-cloud","spotify","tidal","youtube","youtube-music","napster","qobuz","qq-music","vk","anghami","zvuk","gaana","jiosaavn","resso","boomplay"]}'
+var storedSongTitle;
+var rickAstley = document.querySelector("h2")
+var searchObject;
 var tracksEl;
 var listEl = document.getElementById('results-list')
+
 // var resultsHandl;
-//API search request
+
+//puts song name in API format when only song name is stored in local storage.
+function makeSearchObject(){
+    storedSongTitle = localStorage.getItem("apiBodyTitle")
+    console.log("from local storage: " + storedSongTitle)
+    searchObject  = '{"track":"' + storedSongTitle + '","artist":"","type":"track","sources":["amazon-music","apple-music","deezer","pandora","sound-cloud","spotify","tidal","youtube","youtube-music","napster","qobuz","qq-music","vk","anghami","zvuk","gaana","jiosaavn","resso","boomplay"]}'
+    console.log(searchObject)
+    rickAstley.textContent = storedSongTitle + " - Rick Astley"
+}
+
+makeSearchObject();
 
 
-//take that big hairy object, clean it, and put the info we want in the DOM
+//take that big hairy response object, clean it, and put the info we want in the DOM
 
 function renderList(object){
     tracksEl = object.tracks;
@@ -18,40 +30,41 @@ function renderList(object){
     console.log(arr.length)
     for (var i=arr.length-1 ; i >= 0 ; i--){
         if (arr[i].status !== 'success') {
-            console.log("error: " + arr[i].source);
+            // console.log("error: " + arr[i].source);
             arr.splice([i],1);
-            console.log(arr.length);
+            // console.log(arr.length);
         } 
     };
     for (var j=arr.length-1 ; j >= 0 ; j--){
         if (arr[j].data.url === null) {
-            console.log("no URL: "+ arr[j].source);
+            // console.log("no URL: "+ arr[j].source);
             arr.splice([j],1);
-            console.log(arr.length);
+            // console.log(arr.length);
         } 
     };
     console.log(arr);
     for (var k=arr.length-1 ; k >= 0 ; k--){
         if (arr[k].data.name === null){
-            console.log("no trackname: "+ arr[k].source);
+            // console.log("no trackname: "+ arr[k].source);
             arr.splice([k],1);
-            console.log(arr.length);
+            // console.log(arr.length);
         }
 
     };
     for (var l=arr.length-1 ; l >= 0 ; l--){
         if (arr[l].data.artistNames === null){
-            console.log("no ArtistName: "+ arr[l].source);
-            arr[l].data.artistNames = "none given";
-            console.log(arr[l]);
-            console.log(arr.length);
+            // console.log("no ArtistName: "+ arr[l].source);
+            arr[l].data.artistNames = "Artist Name Unknown";
+            // console.log(arr[l]);
+            // console.log(arr.length);
         }
 
     };
     }
     theCleaner(tracksEl);
-    console.log(tracksEl)    
+    // console.log(tracksEl)    
 
+    //this part builds the list from the clean results
     for ( var m=0; m < tracksEl.length; m++){
         var createListItem = document.createElement('li');
         createListItem.style = "font-size: large;"
@@ -81,7 +94,6 @@ function musicAPIcall(){
     
     fetch('https://musicapi13.p.rapidapi.com/search', options)
         .then(response => response.json())
-        // .then(response => console.log(response))
         .then(response => renderList(response))
         .catch(err => console.error(err));   
     
